0f5b829208fa5571fb42b4190505f9b346d8937b
David Sterry <sterry.david@gmail.com>
2021-02-19 10:33:23 -0800

    good pass for delete action creator

--- src/_tests_/actions/index.test.js
+++ src/_tests_/actions/index.test.js
@@ -2,7 +2,7 @@ import * as a from './../../actions';
 
 describe("post list actions", () => {
   it('DELETE_KEG should create DELETE_KEG action', () =>{
-    expect(a.deletePost(1)).toEqual({
+    expect(a.deleteKeg(1)).toEqual({
       type: 'DELETE_KEG',
       id: 1
     });
