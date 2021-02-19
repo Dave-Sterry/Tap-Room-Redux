ee177d1e9924f6e9c857586524871ced4e301415
David Sterry <sterry.david@gmail.com>
2021-02-19 09:33:53 -0800

    pass getting actions into root reducer test

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -22,4 +22,26 @@ describe("rootReducer", () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
 
+  test('check that ADD_KEG action works for kegListReducer and root reducer', () => {
+    const action = {
+      type: "ADD_KEG",
+      name: 'Street Cider',
+      brand: 'VanBeer',
+      price: 3,
+      alcoholContent: "5.3",
+      beer: 23,
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+
+  test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: "TOGGLE_FORM"
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+
 });
