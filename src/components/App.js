8d7a3f03a7aea624a93bfd376194abe20508aed8
David Sterry <sterry.david@gmail.com>
2021-02-19 14:54:50 -0800

    add body component to complete homepage

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Header from './Header';
-import KegControl from './KegControl'
+import KegControl from './KegControl';
+import Body from './Body';
+import {Container} from "react-bootstrap";
 
 function App() {
+
+  const controlStyles ={
+    textAlign: 'center'
+  }
   return (
     <React.Fragment>
       <Header />
-      <KegControl />
+      <Container>
+        <div style = {controlStyles}>
+          <KegControl />
+        </div>
+      </Container>
+        <div style = {controlStyles}>
+          < Body />
+        </div>
     </React.Fragment>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
+
+function Body(){
+  
+
+  return(
+    <React.Fragment>
+      <Container fluid>
+        <h3>Check out some of our fan favorites</h3>
+
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default Body;
