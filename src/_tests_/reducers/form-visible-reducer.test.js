4f9ab1b45b0db24fed187fa252006da2b479ba47
David Sterry <sterry.david@gmail.com>
2021-02-19 08:57:15 -0800

    good fail for toggle action

--- src/_tests_/reducers/form-visible-reducer.test.js
+++ src/_tests_/reducers/form-visible-reducer.test.js
@@ -4,5 +4,9 @@ describe ("formVisibleReducer", () => {
 
   test("should return default state if no action type is recognized", () => {
     expect(formVisibleReducer(false, {type:null})).toEqual(false);
-  })
-})
+  });
+
+  test('should return true if toggle action is passed', () => {
+    expect(formVisibleReducer(false, { type: 'TOGGLE_FORM'})).toEqual(true);
+  });
+});
