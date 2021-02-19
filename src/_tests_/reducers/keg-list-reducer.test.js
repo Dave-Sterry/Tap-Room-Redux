7d73388c647fc41d89b16afd785bb99ebf39a764
David Sterry <sterry.david@gmail.com>
2021-02-19 08:50:39 -0800

    good pass for delete keg

--- src/_tests_/reducers/keg-list-reducer.test.js
+++ src/_tests_/reducers/keg-list-reducer.test.js
@@ -14,6 +14,25 @@ describe('kegListReducer', () => {
   id: 1
 }
 
+const currentState ={
+  1:{
+    name: 'Street Cider',
+    brand: 'VanBeer',
+    price: 3,
+    alcoholContent: "5.3",
+    beer: 23,
+    id: 1
+  },
+  2:{
+    name: 'Dopple Impact',
+    brand: 'VanBeer',
+    price: 4,
+    alcoholContent: "6.8",
+    beer: 120,
+    id: 1
+  }
+}
+
 test('Should successfully add new keg data to masterKegList', () => {
   const {name, brand, price, alcoholContent, beer, id} = kegData;
   action = {
@@ -34,6 +53,23 @@ test('Should successfully add new keg data to masterKegList', () => {
       alcoholContent: alcoholContent, 
       beer: beer, 
       id: id, 
+    }
+  });
+});
+
+test("should delete a keg", () => {
+  action = {
+    type: "DELETE_KEG",
+    id: 1
+  };
+  expect(kegListReducer(currentState, action)).toEqual({
+    2:{
+      name: 'Dopple Impact',
+      brand: 'VanBeer',
+      price: 4,
+      alcoholContent: "6.8",
+      beer: 120,
+      id: 1
     }
   });
 });
