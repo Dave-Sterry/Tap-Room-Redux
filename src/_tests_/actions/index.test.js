1877fe01a261867e175bce993d0f0b33b14ae4ab
David Sterry <sterry.david@gmail.com>
2021-02-19 10:41:44 -0800

    good fail for addkeg creator

--- src/_tests_/actions/index.test.js
+++ src/_tests_/actions/index.test.js
@@ -11,6 +11,17 @@ describe("post list actions", () => {
   it('toggleForm should create TOGGLE_FORM action', () => {
     expect(a.toggleForm()).toEqual({
       type: "TOGGLE_FORM"
-    })
-  })
+    });
+  });
+
+  it('addKeg should create ADD_KEG action', () => {
+    expect(a.addKeg({ name: 'Street Cider', brand: 'VanBeer', price: 3,alcoholContent: "5.3", beer: 23, id: 1})).toEqual({
+      name: 'Street Cider',
+      brand: 'VanBeer',
+      price: 3,
+      alcoholContent: "5.3",
+      beer: 23,
+      id: 1
+    });
+  });
 });
